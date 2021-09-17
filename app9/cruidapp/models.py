8bf534bd4c204f78f66d4f0408777e15510d44b6
https://github.com/Anandp332 <anandp332@gmail.com>
2021-09-17 13:30:26 +0530

    adding models,forms , views,admin

--- app9/cruidapp/models.py
+++ app9/cruidapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
+class Students(models.Model):
+    name = models.CharField(max_length=40)
+    email = models.EmailField()
+    password = models.CharField(max_length=40)
+    address = models.CharField(max_length=100)
+    marks = models.IntegerField()
+    class Meta:
+        db_table = 'Students'
+    def __str__(self):
+        return self.name + "," + self.email + "," + self.address + "," + str(self.marks)
+
+
 # Create your models here.
