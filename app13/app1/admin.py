5761c59a92210814b56a86041dfc7222f580da03
https://github.com/Anandp332 <anandp332@gmail.com>
2021-09-21 16:09:30 +0530

    added models,admin

--- app13/app1/admin.py
+++ app13/app1/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from app1.models import Employee,Dept
+admin.site.register(Employee)
+admin.site.register(Dept)
 # Register your models here.
--- app13/app1/models.py
+++ app13/app1/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.db.models import SET_DEFAULT
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    salary = models.IntegerField()
+    class Meta:
+        verbose_name_plural = 'employee'
+        db_table = 'employee'
+    def __str__(self):
+        return self.name
+
+class Dept(models.Model):
+    name1 = models.CharField(max_length=200)
+    empid = models.ForeignKey(Employee , default=1 , on_delete= models.SET_DEFAULT)
+    exp = models.CharField(max_length=200)
+    class Meta:
+        verbose_name_plural = 'Dept'
+        db_table = 'dept'
+    def __str__(self):
+        return self.name1
+
+
+
 
 # Create your models here.
