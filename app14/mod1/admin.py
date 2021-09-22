0c56f84ca871a7214d682785502b651a9c5c2a5c
https://github.com/Anandp332 <anandp332@gmail.com>
2021-09-22 17:35:51 +0530

    added car and engine model

---
+++ app14/mod1/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from mod1.models import Car,Engine
+admin.site.register(Car)
+admin.site.register(Engine)
+# Register your models here.
---
+++ app14/mod1/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Car(models.Model):
+    reg_no = models.CharField(max_length=10)
+    owner_name = models.CharField(max_length=100)
+
+    class Meta:
+        verbose_name_plural = "Car"
+        db_table = 'car'
+
+    def __str__(self):
+        return self.reg_no
+
+
+class Engine(models.Model):
+    car = models.OneToOneField(Car, on_delete=models.CASCADE, primary_key=True)
+    FuelType = models.CharField(max_length=100)
+    Engine_year = models.IntegerField()
+
+    class Meta:
+        verbose_name_plural = 'Engine'
+        db_table = 'engine'
+
+    def __str__(self):
+        return str(self.Engine_year)
