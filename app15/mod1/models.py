d67b7735b20f8d9f479e988a97793a8ba7cfbfc9
https://github.com/Anandp332 <anandp332@gmail.com>
2021-09-29 16:49:58 +0530

    added models,admin file

--- app15/mod1/models.py
+++ app15/mod1/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
+# Example of One to Many/ Many to One relation
 # Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=10)
+    year = models.IntegerField()
+    ratings = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+
+class AudioSongs(models.Model):
+    name = models.CharField(max_length=10)
+    length = models.IntegerField()
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
